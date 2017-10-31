5db1dd0dddfb831154c60a8261b4c900a10d425e
Rafael B. Tauil <rafael@tauil.com.br>
2017-10-31 16:43:22 -0200

    Improve forms layout and adds relation fields to new record components

--- src/app/modules/patient/components/new-patient/new-patient.component.ts
+++ src/app/modules/patient/components/new-patient/new-patient.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PatientService } from '../../services/patient.service';
 import { Patient } from '../../patient';
+import { ClinicService } from '../../../clinic/services/clinic.service';
+import { Clinic } from '../../../clinic/clinic';
+import { TherapistService } from '../../../therapist/services/therapist.service';
+import { Therapist } from '../../../therapist/therapist';
 
 @Component({
   selector: 'new-patient',
@@ -11,16 +15,37 @@ import { Patient } from '../../patient';
 export class NewPatientComponent implements OnInit {
 
   patient: Patient = {id: null, name: '', clinic_id: null, therapist_id: null};
+  clinics: Array<Clinic>;
+  therapists: Array<Therapist>;
 
-  constructor(private patientService: PatientService, public router: Router) { }
+  constructor( private patientService: PatientService,
+               private router: Router,
+               private clinicService: ClinicService,
+               private therapistService: TherapistService ) { }
 
   ngOnInit() {
+    this.loadClinics();
+    this.loadTherapists();
   }
 
   save() {
     this.patientService.create(this.patient).subscribe(patient => {
       this.router.navigate(['/patients/' + patient.id]);
     });
+  }
+
+  loadClinics() {
+    this.clinicService.list().subscribe(
+      clinics => (this.clinics = clinics),
+      error => console.log(error)
+    );
+  }
+
+  loadTherapists() {
+    this.therapistService.list().subscribe(
+      therapists => (this.therapists = therapists),
+      error => console.log(error)
+    );
   }
 
 }
--- src/app/modules/therapist/components/new-therapist/new-therapist.component.ts
+++ src/app/modules/therapist/components/new-therapist/new-therapist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TherapistService } from '../../services/therapist.service';
 import { Therapist } from '../../therapist';
+import { ClinicService } from '../../../clinic/services/clinic.service';
+import { Clinic } from '../../../clinic/clinic';
 
 @Component({
   selector: 'new-therapist',
@@ -11,16 +13,27 @@ import { Therapist } from '../../therapist';
 export class NewTherapistComponent implements OnInit {
 
   therapist: Therapist = {id: null, name: '', clinic_id: null};
+  clinics: Array<Clinic>;
 
-  constructor(private therapistService: TherapistService, public router: Router) { }
+  constructor( private therapistService: TherapistService,
+               private router: Router,
+               private clinicService: ClinicService ) { }
 
   ngOnInit() {
+    this.loadClinics();
   }
 
   save() {
     this.therapistService.create(this.therapist).subscribe(therapist => {
       this.router.navigate(['/therapists/' + therapist.id]);
     });
+  }
+
+  loadClinics() {
+    this.clinicService.list().subscribe(
+      clinics => (this.clinics = clinics),
+      error => console.log(error)
+    );
   }
 
 }
